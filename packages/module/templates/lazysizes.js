bc1a099a1c19f3df0b65169b8c545a9693497bc1
Matthew Wagerfield <matthew@wagerfield.com>
2019-02-22 21:41:39 +0000

    fix: import statement formatting

--- packages/module/templates/lazysizes.js
+++ packages/module/templates/lazysizes.js
@@ -3,11 +3,12 @@
   "optimumx",
   "object-fit",
   "parent-fit"
-]).forEach((plugin) => {%>import "lazysizes/plugins/<%=plugin%>/ls.<%=plugin%>"<% }) %>
+]).forEach((plugin) => {%>
+import "lazysizes/plugins/<%=plugin%>/ls.<%=plugin%>"<% }) %>
 import lazysizes from "lazysizes"
 
 export default (context) => {
-  let opts = <%= serialize(options.options) %>
+  let opts = <%= serialize(options.options || {}) %>
   if (typeof opts === "function") opts = opts(context)
 
   Object.assign(lazysizes.cfg, opts)
